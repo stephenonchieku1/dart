19c5d6f8b6ba22d01d18f93a8dede24e4ba9954c
stephenonchieku1 <onchiekustephen901@gmail.com>
2024-01-27 19:50:10 +0300

    datatype number

--- variables.dart
+++ variables.dart
@@ -1,4 +1,15 @@
 main (){
-  int number = 5;  
-   print(number );
+  // num  type
+  num firstNo= 25;
+  num secondNo=17;
+
+  int  hex = 0xda34f;
+ int  five= 5;
+   
+  double pi = 3.142;
+   print(five);
+   print(pi);
+  print(hex + pi);
+   print(firstNo);
+   print(secondNo );
 }
