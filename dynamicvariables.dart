d6c398471f3740f294469ca87e8f83ee049b3c57
stephenonchieku1 <onchiekustephen901@gmail.com>
2024-01-29 18:44:47 +0300

    dynamic variables

---
+++ dynamicvariables.dart
@@ -0,0 +1,13 @@
+main() {
+  // you can use dynamic keyword to declare a variable that can hold objects of many types : ie string ,number,bool in the same variable
+  dynamic d = new DateTime.now();
+  print(d);
+  d = 5;
+  print(d);
+  d = "student";
+  print(d);
+  d = null;
+  print(d);
+   d = false;
+   print(d);
+}
