5bbc2d1fcfe31ddbd4328b21a56ea488dde0d53c
stephenonchieku1 <onchiekustephen901@gmail.com>
2024-01-30 19:33:42 +0300

    increment operator

--- arithmetics.dart
+++ arithmetics.dart
@@ -4,9 +4,12 @@ main() {
 
   print(operand1 + operand2);
   print(operand1 - operand2);
-  print(- operand1);
+  print(-operand1);
   print(operand1 * operand2);
   print(operand1 / operand2);
   print(operand1 ~/ operand2);
   print(operand1 % operand2);
+  //increment operator
+  var age = 5;
+  print(++age);
 }
