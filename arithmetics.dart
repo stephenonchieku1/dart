0d09bfdf6b1648bd8fa9489da71237837de56e01
stephenonchieku1 <onchiekustephen901@gmail.com>
2024-01-30 19:31:22 +0300

    arithmetic operators

---
+++ arithmetics.dart
@@ -0,0 +1,12 @@
+main() {
+  var operand1 = 10;
+  var operand2 = 7;
+
+  print(operand1 + operand2);
+  print(operand1 - operand2);
+  print(- operand1);
+  print(operand1 * operand2);
+  print(operand1 / operand2);
+  print(operand1 ~/ operand2);
+  print(operand1 % operand2);
+}
