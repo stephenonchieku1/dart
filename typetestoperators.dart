250e957a5de3e82a573cb0ca0304536f48ead5b7
stephenonchieku1 <onchiekustephen901@gmail.com>
2024-02-01 21:33:24 +0300

    updated

--- typetestoperators.dart
+++ typetestoperators.dart
@@ -11,5 +11,17 @@ void main(List<String> args) {
   print(type4 is double);
   print(type4 is! double);
 
+  // is operator
+  double type1 = 5.0;
+  int type2 = 87;
+  String type3 = "educative";
+  bool type4 = true;
+
+  print(type1 is int);
+  print(type2 is int);
+  print(type3 is String);
+  print(type4 is double);
+  print(type4 is! double);
+  
   
 }
